f357e4f29c11e5717265d9e1d2991e3f83553f79
Aaron Rosenberg <aaronrbg@gmail.com>
2018-10-30 21:28:45 +0000

    added post and urls_new

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 //load the things we need
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -24,11 +26,20 @@ app.get('/urls', function(req, res) {
     res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
 // url list page 
 app.get('/urls/:id', function(req, res) {
     let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
     res.render('urls_show', templateVars);
 });
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // debug statement to see POST parameters
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
 
 
 app.listen(PORT, () => {
