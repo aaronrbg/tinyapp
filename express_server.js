83bb5ad9d8f23bcc4db4c602738ef4d1ed1feb83
Aaron Rosenberg <aaronrbg@gmail.com>
2018-11-02 03:04:10 +0000

    finished core functionality. onto cleanup and refactoring....

--- express_server.js
+++ express_server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
 const bcrypt = require('bcrypt');
 const cookieSession = require('cookie-session')
 app.use(bodyParser.urlencoded({extended: true}));
